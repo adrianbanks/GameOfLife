70fe5c15295ceb71ba9e62c9fe60577ce3e7427a
Adrian Banks <github@adrianbanks.co.uk>
2021-05-07 13:16:49 +0100

    Initial code structure

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adrianbanks.GameOfLife
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
