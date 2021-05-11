3d6e471a6412900d26ed9e45fa176a2519d753c8
Adrian Banks <github@adrianbanks.co.uk>
2021-05-11 21:17:31 +0100

    Handle when a QR code can't be found

--- GameOfLife/Boards/QrCodes/QrCodeParser.cs
+++ GameOfLife/Boards/QrCodes/QrCodeParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.IO;
 using ZXing;
 using ZXing.Common;
@@ -12,16 +13,22 @@ namespace adrianbanks.GameOfLife.Boards.QrCodes
             using var stream = File.OpenRead(path);
             using var image = new Bitmap(stream);
 
-            var matrix = GetCellMatrix(image);
+            var matrix = GetCellMatrix(path, image);
             return MatrixToBoardConverter.Convert(matrix);
         }
 
-        public static BitMatrix GetCellMatrix(Bitmap image)
+        public static BitMatrix GetCellMatrix(string path, Bitmap image)
         {
             var luminanceSource = new BitmapLuminanceSource(image);
             var binarizer = new HybridBinarizer(luminanceSource);
             var bitmap = new BinaryBitmap(binarizer);
             var result = new ZXing.QrCode.Internal.Detector(bitmap.BlackMatrix).detect();
+
+            if (result == null)
+            {
+                throw new Exception($"Could not find QR code in '{path}'");
+            }
+
             return result.Bits;
         }
     }
