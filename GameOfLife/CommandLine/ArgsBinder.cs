c2062c185c1cd2e7f28cbafe0c12cbaa174048f7
Adrian Banks <adrian.banks@red-gate.com>
2022-01-04 16:57:43 +0000

    Update System.CommandLine
    See https://github.com/dotnet/command-line-api/issues/1537

---
+++ GameOfLife/CommandLine/ArgsBinder.cs
@@ -0,0 +1,45 @@
+using System.CommandLine;
+using System.CommandLine.Binding;
+
+namespace adrianbanks.GameOfLife.CommandLine
+{
+    internal sealed class ArgsBinder : BinderBase<Args>
+    {
+        private readonly Option<int?> width = new(new[] {"--width", "-w"}, "The width of the grid");
+        private readonly Option<int?> height = new(new[] {"--height", "-h"}, "The height of the grid");
+        private readonly Option<int> iterations = new(new[] {"--iterations", "-i"}, () => Defaults.Iterations, "The number of iterations to perform");
+        private readonly Option<int> initialDelay = new(new[] {"--initial-delay", "-id"}, () => Defaults.InitialDelay, "The delay for which the first iteration is shown (in milliseconds)");
+        private readonly Option<int> delay = new(new[] {"--delay", "-d"}, () => Defaults.Delay, "The delay between each iteration (in milliseconds)");
+        private readonly Option<string> backColor = new(new[] {"--back-color", "-b"}, () => Defaults.BackColor, "Sets the background color to use for rendering");
+        private readonly Option<string> baseColor = new(new[] {"--base-color", "-c"}, () => Defaults.BaseColor, "Sets the base color to use for rendering");
+        private readonly Option<string> pattern = new(new[] {"--pattern", "-p"}, "Generates a starting point using a known pattern");
+        private readonly Option<string> qrCode = new(new[] {"--qr-code", "-q"}, "The file path of an image containing a QR code");
+
+        protected override Args GetBoundValue(BindingContext bindingContext) =>
+            new()
+            {
+                Width = bindingContext.ParseResult.GetValueForOption(width),
+                Height = bindingContext.ParseResult.GetValueForOption(height),
+                Iterations = bindingContext.ParseResult.GetValueForOption(iterations),
+                InitialDelay = bindingContext.ParseResult.GetValueForOption(initialDelay),
+                Delay = bindingContext.ParseResult.GetValueForOption(delay),
+                BackColor = bindingContext.ParseResult.GetValueForOption(backColor),
+                BaseColor = bindingContext.ParseResult.GetValueForOption(baseColor),
+                Pattern = bindingContext.ParseResult.GetValueForOption(pattern),
+                QrCode = bindingContext.ParseResult.GetValueForOption(qrCode)
+            };
+
+        public void InitialiseOptions(Command command)
+        {
+            command.Add(width);
+            command.Add(height);
+            command.Add(iterations);
+            command.Add(initialDelay);
+            command.Add(delay);
+            command.Add(backColor);
+            command.Add(baseColor);
+            command.Add(pattern);
+            command.Add(qrCode);
+        }
+    }
+}
--- GameOfLife/CommandLine/CommandLine.cs
+++ GameOfLife/CommandLine/CommandLine.cs
@@ -1,7 +1,6 @@
 using System;
 using System.CommandLine;
 using System.CommandLine.Builder;
-using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
 
 namespace adrianbanks.GameOfLife.CommandLine
@@ -17,34 +16,18 @@ namespace adrianbanks.GameOfLife.CommandLine
                 TreatUnmatchedTokensAsErrors = true
             };
 
-            var runCommand = new Command("run", "Runs the game")
-            {
-                TreatUnmatchedTokensAsErrors = true,
-                Handler = CommandHandler.Create(runGame)
-            };
-            runCommand.AddOption(new Option<int?>(new[] { "--width", "-w" }, "The width of the grid"));
-            runCommand.AddOption(new Option<int?>(new[] { "--height", "-h" }, "The height of the grid"));
-            runCommand.AddOption(new Option<int>(new[] { "--iterations", "-i" }, () => Defaults.Iterations, "The number of iterations to perform"));
-            runCommand.AddOption(new Option<int>(new[] { "--initial-delay", "-id" }, () => Defaults.InitialDelay, "The delay for which the first iteration is shown (in milliseconds)"));
-            runCommand.AddOption(new Option<int>(new[] { "--delay", "-d" }, () => Defaults.Delay, "The delay between each iteration (in milliseconds)"));
-            runCommand.AddOption(new Option<string>(new[] { "--back-color", "-b" }, () => Defaults.BackColor, "Sets the background color to use for rendering"));
-            runCommand.AddOption(new Option<string>(new[] { "--base-color", "-c" }, () => Defaults.BaseColor, "Sets the base color to use for rendering"));
-            runCommand.AddOption(new Option<string>(new[] { "--pattern", "-p" }, "Generates a starting point using a known pattern"));
-            runCommand.AddOption(new Option<string>(new[] { "--qr-code", "-q" }, "The file path of an image containing a QR code"));
+            var argsBinder = new ArgsBinder();
+            var runCommand = new Command("run", "Runs the game") { TreatUnmatchedTokensAsErrors = true };
+            argsBinder.InitialiseOptions(runCommand);
+            runCommand.SetHandler(runGame, argsBinder);
             rootCommand.Add(runCommand);
 
-            var showPatternsCommand = new Command("show-patterns", "Shows the available known patterns")
-            {
-                TreatUnmatchedTokensAsErrors = true,
-                Handler = CommandHandler.Create(showPatterns)
-            };
+            var showPatternsCommand = new Command("show-patterns", "Shows the available known patterns") { TreatUnmatchedTokensAsErrors = true };
+            showPatternsCommand.SetHandler(showPatterns);
             rootCommand.AddCommand(showPatternsCommand);
 
-            var showColorsCommand = new Command("show-colors", "Shows the available base colors to use for rendering")
-            {
-                TreatUnmatchedTokensAsErrors = true,
-                Handler = CommandHandler.Create(showColors)
-            };
+            var showColorsCommand = new Command("show-colors", "Shows the available base colors to use for rendering") { TreatUnmatchedTokensAsErrors = true };
+            showColorsCommand.SetHandler(showColors);
             rootCommand.AddCommand(showColorsCommand);
 
             return new CommandLineBuilder(rootCommand)
